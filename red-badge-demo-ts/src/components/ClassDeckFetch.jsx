a9f2a5bd2c6f8fdd189a630e89c4aa38ea2769e5
jeff kaplan <jkaplan049@gmail.com>
2021-03-25 20:41:40 -0400

    update

--- red-badge-demo-ts/src/components/ClassDeckFetch.jsx
+++ red-badge-demo-ts/src/components/ClassDeckFetch.jsx
@@ -1,41 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ClassDeckFetch extends Component {
-    constructor(props){
-        super(props);
-        this.state = {deckID: ""}
-    
-    }
-    fetchDeck() {
-        // what is Url
-        // store the data
-const url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+  constructor(props) {
+    super(props);
+    this.state = { deckID: "", cardImage: "" };
+  }
+  fetchDeck() {
+    //Whats url are we fetching?
+    //Store the data from the fetch
+    const url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
 
-fetch(url)
-.then((res) => res.json())
-.then((json) => this.setState({deckID: json.deck_ID}))
-    }
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => this.setState({ deckID: json.deck_id }));
+  }
 
-    fetchCard() {
-const url = `https://deckofcardsapi.com/api/deck/${this.state.deckID}/draw/?count=2
-`
-fetch(url)
-.then((res) => res.json())
-.then((json) => console.log(json))
-    }
+  fetchCard() {
+    const url = `https://deckofcardsapi.com/api/deck/${this.state.deckID}/draw/?count=2`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => this.setState({ cardImage: json.cards[0].image }));
+  }
 
-    componentDidMount() {
-        this.fetchDeck();
-    }
+  componentDidMount() {
+    this.fetchDeck();
+  }
 
-    componentDidUpdate(prevProps, prevState) {
-      
-        if(prevState.deckID !== this.state.deckID){
-            this.fetchCard();
-        }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.deckID !== this.state.deckID) {
+      this.fetchCard();
     }
-    render() {
-        return <div>Hello from ClassDeckFetch</div>
-    }
+  }
+  render() {
+    return (
+      <div>
+        <img src={this.state.cardImage} alt="" />
+      </div>
+    );
+  }
 }
+
 export default ClassDeckFetch;
